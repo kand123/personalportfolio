bb82015670670f2ac8394a2c963aeec2211185f5
Kristin <kbrandtsojo@gmail.com>
2019-02-25 19:46:59 -0700

    Added images and data files

---
+++ main.js
@@ -0,0 +1,19 @@
+ - ES 6 - latest javascript 
+ variable - container that stores a value 
+ var myVariable -declared a variable, but not initialized
+(old)
+ let thisVariable = 'thor' equal sign is an operator single equal sign assignment operator
+ const ANSWER = 42 
+(new)
+
+good resource on mdn 
+
+=== quality checking
+
+array [] uses square brackets 
+let minArray can store info - different types of number, objects, other arrays, boolean, etc.
+
+0 based - first item is 0, and so on 
+
+myArray[0] points to the first 
+
