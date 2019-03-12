30925d00586050e4e829f2088c27c2d0c15f0328
Kristin <kbrandtsojo@gmail.com>
2019-03-12 10:08:23 -0600

    working through home planets

--- starWarsPeople.js
+++ starWarsPeople.js
@@ -1,4 +1,5 @@
 import { people } from '../data/people.js'
+import { planets } from '../data/planets.js'
 
 const men = people.filter(person => person.gender === 'male')
 const women = people.filter(person => person.gender === 'female')
@@ -6,10 +7,40 @@ const other = people.filter(person => (person.gender === 'n/a') || (person.gende
 
 //console.log (men, women, other)
 
+const allHomeWorlds = people.map (person => {
+    let foundWorld = planets.find(element => {
+        return element.url === person.homeworld
+    })
+    return { name: person.name, home: person.foundworld}
+})
+
+console.log(allHomeWorlds)
+
+//const peopleAndPlanets = [...people, ...planets]
+
+//console.log(peopleAndPlanets)
+
+//const homeWorlds = peopleAndPlanets.find((element) => {
+  //  return element.url === element.homeworld
+//})
+
+//console.log(homeWorld)
+
 const mainContainer = document.createElement('div')
 mainContainer.className = 'container'
 
 men.forEach((man) => {
+    let manElement = document.createElement('div')
+    manElement.className = 'box'
+    manElement.textContent = man.name
+    mainContainer.appendChild(manElement)
+    let eyeColor = document.createElement('p')
+    eyeColor.textContent = man.eye_color
+    manElement.appendChild(eyeColor)
+    mainContainer.appendChild(manElement)
+})
+
+women.forEach((man) => {
     let manElement = document.createElement('div')
     manElement.className = 'box'
     manElement.textContent = man.name
