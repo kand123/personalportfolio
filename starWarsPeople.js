4a2b0c590b482c6b8b1398c76160ff7a99d8cb11
Kristin <kbrandtsojo@gmail.com>
2019-03-18 12:44:06 -0600

    images

--- starWarsPeople.js
+++ starWarsPeople.js
@@ -1,24 +1,29 @@
 import { people } from '../data/people.js'
 import { planets } from '../data/planets.js'
 
-const allHomeWorlds = people.map(person => {
-    let foundWorld = planets.find(planet => {
-        return planet.url === person.homeworld 
-    })
+
 
     const getLastNumber = (url) => {
         let end = url.lastIndexOf('/')
-        
         let start = end -2
-        console.log(start + ' ' + end)
+
+        if(url.charAt(start) === '/') {
+            start++
+        }
+        console.log(url.slice (start, end))
     }
     
+    const allHomeWorlds = people.map(person => {
+        let foundWorld = planets.find(planet => {
+            return planet.url === person.homeworld 
+        })
 
-    getLastNumber(person.url)
+    let imageURL = getLastNumber(person.url)
     return {
         name: person.name,
         home: foundWorld.name,
-        eye_color: person.eye_color
+        eye_color: person.eye_color,
+        imagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`,
     }
 })
 //console.log(allHomeWorlds)
@@ -32,12 +37,16 @@ mainContainer.className = 'container'
 allHomeWorlds.forEach((person) => {
     let personElement = document.createElement('div')
     let planetElement = document.createElement('p')
+    let imageElement = document.createElement ('img')
+
     personElement.className = 'box'
     personElement.textContent = person.name
     planetElement.textContent = person.home
+    imageElement.src = person.imagePath
    
    
    personElement.appendChild(planetElement)
+   personElement.appendChild(imageElement)
     mainContainer.appendChild(personElement)
 })
 
